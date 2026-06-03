d982e56a3e8c9569b4dbb98c74842d05ddf94db3
BlackLight <bl.blacklight01@gmail.com>
2026-06-03 21:33:34 +0600

    added neon db config

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(bind=engine)
 
