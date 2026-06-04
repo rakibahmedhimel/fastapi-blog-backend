d009e9af2044e2d1c24991ceae8c2a128b5a61eb
BlackLight <bl.blacklight01@gmail.com>
2026-06-04 18:51:59 +0600

    bla

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine, Base
 from app.routers import user, post, comment, like
 from app import models
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  # React frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(user.router)
 app.include_router(post.router)
