c706804ae75e36f4ff87387e8bd3f91e8a95470e
BlackLight <bl.blacklight01@gmail.com>
2026-06-22 19:53:03 +0600

    vercel error fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 import Login from "./pages/Login";
